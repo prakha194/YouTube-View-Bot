be0ca9e1060b3edfaf66bc2df20107e0b8032bfe
prakha194 <mgbruuhh@gmail.com>
2025-03-02 21:42:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+# YouTube Channel ID (Your Channel)
+YOUTUBE_CHANNEL_ID = "UCgK8BDET9UfKYFAJ4UiLZOA"
+
+# Windscribe VPN Credentials
+WINDSCRIBE_USERNAME = "YOUR_WINDCRIBE_EMAIL"
+WINDSCRIBE_PASSWORD = "YOUR_WINDCRIBE_PASSWORD"
+
+# Max views per video (to avoid detection)
+MAX_VIEWS = 5  
+
+# Use Free Proxy List (Fallback if VPN fails)
+USE_FREE_PROXIES = True
